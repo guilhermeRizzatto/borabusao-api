f7afe158c59e83f82e7d102952a7602faf96e463
Guilherme Teixeira <guilherme.teixeira@lh.com.br>
2026-08-14 21:00:37 -0300

    Primeiro commit

---
+++ src/main/java/com/rizzatto/borabusao_api/BorabusaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rizzatto.borabusao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorabusaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorabusaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizzatto/borabusao_api/BorabusaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizzatto.borabusao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorabusaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
